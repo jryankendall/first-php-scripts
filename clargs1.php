31e6f1838e91c8fb17fc6716943b63a2fa289a82
JKRyan <jed@jedkendall.com>
2019-12-02 11:00:06 -0600

    Initial commit, test files and such.

---
+++ clargs1.php
@@ -0,0 +1,5 @@
+<?php
+echo "Test Arguments:\n";
+echo $_SERVER["argc"]."\n";
+echo $_SERVER["argv"][0]."\n";
+?>
---
+++ helloworld.php
@@ -0,0 +1,4 @@
+#!C:\Program Files\php\php.exe -q
+<?php
+echo "Hello world of PHP CLI!";
+?>
